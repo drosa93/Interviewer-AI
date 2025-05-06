394eb15da80b7f5164e9521967920cccbe964548
drosa93 <DM_delarosa93@yahoo.com>
2025-05-06 12:28:02 -0500

    welcome page

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
 <div>
-<h1>We in this bitch ain't it?</h1>
+<h1>Hello & welcome to the Ai interviewer app</h1>
 <Button>Subscribe</Button>
 </div>
   );
