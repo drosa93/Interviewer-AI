8c3161a7ca2e10f56c85d09a3c403dff12764046
drosa93 <DM_delarosa93@yahoo.com>
2025-05-06 12:48:57 -0500

    fixed front page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link"
 
 export default function Home() {
   return (
 <div>
 <h1>Hello & welcome to the Ai interviewer app</h1>
-<Button>Subscribe</Button>
+<Link href="/auth">
+<Button>Log-in</Button>
+</Link>
 </div>
   );
 }
